485ea82d938f48ba5d9e80cf25514edddc200166
bitfl0wer <florian@pro-weber.com>
2023-02-12 22:08:43 +0100

    Fix Typo

--- src/cog.py
+++ src/cog.py
@@ -5,14 +5,13 @@ from discord.commands import (
 )
 
 
-class ExampleCog(commands.cog):
+class ExampleCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-
-@slash_command(name="hello")
-async def hello_world(self, ctx: discord.Interaction):
-    await ctx.response.send_message(f"Hello, World!", ephemeral=True)
+    @slash_command(name="hello")
+    async def hello_world(self, ctx: discord.Interaction):
+        await ctx.response.send_message(f"Hello, World!", ephemeral=True)
 
 
 def setup(bot: discord.Bot):
