249930cc13142def79e74984bbc11a7ae61ea10f
bitfl0wer <florian@pro-weber.com>
2023-02-13 18:29:33 +0100

    Scrap previous attempt at writing the command

--- src/commands/temporality.py
+++ src/commands/temporality.py
@@ -1,25 +1,13 @@
 import discord
 from discord.ext import commands
 from discord.commands import slash_command, Option
-
-# 1: User types command
-# 2: USer gets asked
+from src.database_handler import session as db
 
 temporality_mode = str()
 
 
 def temporality_logic(mode: str) -> None:
     return
-
-
-class DropdownView(discord.ui.View):
-    @discord.ui.channel_select(
-        placeholder="Select channels...", min_values=1, max_values=25
-    )
-    async def channel_select_dropdown(
-        self, select: discord.ui.Select, interaction: discord.Interaction
-    ) -> None:
-        temporality_logic(temporality_mode)
 
 
 class Temporality(commands.Cog):
@@ -30,6 +18,10 @@ class Temporality(commands.Cog):
     async def temporality_setup(
         self,
         ctx: discord.ApplicationContext,
+        channel: Option(
+            discord.TextChannel,
+            description="The channel to activate automatically disappearing messages in.",
+        ),
         time: Option(
             str,
             description="The amount of time the messages should get deleted after. Examples: 30min, 7h, 5d, 2w, 3m, 1y",
