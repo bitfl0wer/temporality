687d66d2924d529b4da4bccf346ebed79e7ab777
bitfl0wer <florian@pro-weber.com>
2023-02-13 22:21:19 +0100

    Quit for today.

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,40 @@
-import discord
+import discord, time, threading
 from os import environ
 from dotenv import load_dotenv
 from discord.ext import commands
+from src.database_handler import session as db, Channels, Messages
+from src.commands.temporality import (
+    seperate_str_and_int,
+    make_relative_timestamp,
+)
+
+
+def temporality_logic() -> None:
+    messages = db.query(Messages).all()
+    for message in messages:
+        if message.deletion_timestamp <= int(time.time()):
+            channel = db.query(Channels).filter_by(id=message.channel_id).one_or_none()
+            if channel.active:
+                channel = bot.get_channel(channel.id)
+                message = channel.fetch_message(message.id)
+                message.delete()
+            db.delete(message)
+            db.commit()
+    return
+
+
+def run_periodically(func, interval_seconds):
+    def run_func():
+        while True:
+            func()
+            t = threading.Timer(interval_seconds, run_func)
+            t.start()
+            t.join()
+            return
+
+    t = threading.Thread(target=run_func)
+    t.start()
+
 
 load_dotenv()
 
@@ -21,5 +54,27 @@ async def on_ready():
     print(f"Logged in as {bot.user}.")
 
 
+@bot.event
+async def on_message(message: discord.message):
+    if message.author.id == bot.user.id:
+        return
+
+    if not db.query(Channels).filter_by(id=message.channel.id).first():
+        return
+
+    timeout = seperate_str_and_int(
+        db.query(Channels.timeout).filter_by(id=message.channel.id).one_or_none()[0]
+    )
+
+    expiration_date = make_relative_timestamp(timeout[0], timeout[1])
+
+    db_message = Messages(
+        id=message.id, channel_id=message.channel.id, deletion_timestamp=expiration_date
+    )
+    db.add(db_message)
+    db.commit()
+
+
 bot.load_extension("src.commands.temporality")
+run_periodically(temporality_logic, 1)
 bot.run(__token__)
--- src/commands/temporality.py
+++ src/commands/temporality.py
@@ -1,13 +1,59 @@
-import discord
+from typing import List, Union
+
+import discord, time
+from discord.commands import Option, slash_command
 from discord.ext import commands
-from discord.commands import slash_command, Option
+
+from src.database_handler import Channels, Messages
 from src.database_handler import session as db
 
-temporality_mode = str()
+allowed_units_time = ["s", "min", "h", "d", "w", "m", "y"]
 
 
-def temporality_logic(mode: str) -> None:
-    return
+def seperate_str_and_int(input: str) -> list:
+    """Given an example string '123hello', this function will split this string into a list
+    containing the elements [123] and ['hello'].
+
+    Args:
+        input (str): a string that starts with numbers which are followed by chars.
+
+    Returns:
+        List[str, int]: The output list
+    """
+    i = 0
+    _output = list()
+    for char in input:
+        if not char.isdigit():
+            _output.append(input[i:])
+            _output.append(input[0:i])
+            return _output
+        else:
+            i += 1
+
+
+def make_relative_timestamp(unit: str, time_in_future: int) -> int:
+    """Creates a timestamp that is $time seconds in the future.
+
+    Args:
+        unit (str): either s, min, h, d, w, m or y.
+        time (int): positive integer
+
+    Returns:
+        int: unix timestamp
+    """
+    if not unit in allowed_units_time:
+        return
+    current_time = int(time.time())
+    unit_multiplier_dict = {
+        "s": 1,
+        "min": 60,
+        "h": 3600,
+        "d": 86400,
+        "w": 604800,
+        "m": 2629800,
+        "y": 31556952,
+    }
+    return current_time + (int(time_in_future) * unit_multiplier_dict.get(unit))
 
 
 class Temporality(commands.Cog):
@@ -27,7 +73,24 @@ class Temporality(commands.Cog):
             description="The amount of time the messages should get deleted after. Examples: 30min, 7h, 5d, 2w, 3m, 1y",
         ),
     ):
-        await ctx.response.send_message(f"Hello, World!", ephemeral=True)
+        if not ctx.user.guild_permissions.manage_messages:
+            await ctx.response.send_message(
+                f"womp womp. You do not have the required `MANAGE_MESSAGES` permission.",
+                ephemeral=True,
+            )
+        if not seperate_str_and_int(time)[0] in allowed_units_time:
+            await ctx.response.send_message(
+                f"Time unit not recognized. Please use either `s, min, h, d, w, m` or `y`.",
+                ephemeral=True,
+            )
+            return
+        db_channel = Channels(id=channel.id, timeout=time)
+        db.add(db_channel)
+        db.commit()
+        await ctx.response.send_message(
+            f"Disappearing messages activated for channel #{channel.name} with a timer of {time}!"
+        )
+        return
 
 
 def setup(bot: discord.Bot):
